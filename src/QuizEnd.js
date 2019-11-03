367261655b2db6f08e67a9db0b61524271d0ed92
thierabsy <thierabsy@yahoo.fr>
2019-11-03 02:00:43 +0100

    Run build

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -8,7 +8,7 @@ class QuizEnd extends Component {
 
 	render(){
 		return(
-			<div>
+			<div className="QuizEnd">
 				<img src="/assets/imgs/checksuccess.png" alt="Submit Success" />
 				<p>Thanks for playing!</p>
 				<a 
